ca9f1619018e8a6c286df6d7454f28ade97b74cb
Toon <85719086+Toon-arch@users.noreply.github.com>
2021-08-16 19:25:58 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -21,7 +21,7 @@ local Import = function(Asset)
 	if type(Asset) == "number" then
 		return game:GetObjects("rbxassetid://" .. Asset)[1]
 	else
-		local Link = string.format("https://raw.githubusercontent.com/Toon-arch/Fadeti/main/main.lua/%s", Asset)
+		local Link = string.format("https://raw.githubusercontent.com/Toon-arch/Fadeti/main/Files/%s", Asset)
 		local Response = game:HttpGetAsync(Link)
 		local Function = loadstring(Response)
 		local Success, Return = pcall(Function)
@@ -35,6 +35,37 @@ end
 
 local Serv = setmetatable({}, {__index = function(self, index) return game:GetService(index) end})
 local Players = Serv.Players
+local RunService = Serv.RunService
+
+local AddCon = function(name, notif, callback)
+	name = name or "Name"
+	callback = callback or function() end
+	env().Fadeti_Events[tostring(name)] = RunService.Stepped:Connect(function()
+		pcall(callback)
+	end)
+	if notif ~= nil then
+		coroutine.wrap(notify)(tostring(notif))
+	end
+end
+
+local DisCon = function(name, notif)
+	name = name or "Name"
+	for index, value in pairs(env().Fadeti_Events) do
+		if value and typeof(value) == "RBXScriptConnection" and tostring(index) == tostring(name) then
+			value:Disconnect()
+		end
+	end
+	if notif ~= nil then
+		coroutine.wrap(notify)(tostring(notif))
+	end
+end
+
+
+
+
+
+
+
 
 local version = "1.0"
 local plugin = {}
@@ -43,11 +74,31 @@ plugin.PluginName = string.format("Fadeti v%s", version)
 plugin.PluginDescription = ""
 plugin.Commands = {}
 
+plugin.Commands["iyconsole"] = {
+	ListName = ("iyconsole / iycon"),
+	Description = ("Load Hunter's IY Console Script to run Infinite Yield commands in a rconsole window"),
+	Aliases = {"iycon"},
+	Function = function(args, speaker)
+		Import("huntersRconsole.lua")
+	end
+}
+
+return plugin
+
+
+
+
+
+
+
+
+--[[
 plugin.Commands["example"] = {
 	ListName = ("name"),
 	Description = ("desc"),
 	Aliases = {},
-	Function = function(arguments, speaker)
-
+	Function = function(args, speaker)
+		
 	end
-};
+}
+]]--
